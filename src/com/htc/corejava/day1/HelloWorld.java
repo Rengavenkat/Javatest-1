7081a9a452eaa8b503d87afa560b1758a3802d3b
Your Name <you@example.com>
2023-04-06 19:34:34 -0500

    Test-Commit-1

---
+++ src/com/htc/corejava/day1/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.htc.corejava.day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String s = " Hello Java world ";
+		
+		//System.out.println(s.indexOf("v",8));
+		//System.out.println(s.toLowerCase());
+		System.out.println(s.trim());
+	}
+
+}
