f50ae00557736538ce281e646b80947c0d40174d
Rengavenkat <129563422+Rengavenkat@users.noreply.github.com>
2023-04-10 19:19:28 -0500

    Update HelloWorld.java

--- src/com/htc/corejava/day1/HelloWorld.java
+++ src/com/htc/corejava/day1/HelloWorld.java
@@ -3,7 +3,7 @@ package com.htc.corejava.day1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		String s = " Hello Java world! This is Renga ";
+		String s = " Hello Java world! This is Renganayaki ";
 		
 		//System.out.println(s.indexOf("v",8));
 		//System.out.println(s.toLowerCase());
